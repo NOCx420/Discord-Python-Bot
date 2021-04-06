04d3ff173561735a5f310e82557fe21e77cce461
NOCx420 <56538139+NOCx420@users.noreply.github.com>
2021-04-06 20:40:23 +0200

    Uploaded project files

---
+++ config.py
@@ -0,0 +1,19 @@
+status = ["with Anime Boobies",
+          "with your Mom",
+          "Deus Ex: Human Revolution",
+          "with Roadkill",
+          "Cyberbonk2088",
+          "Minecraft on Pornhub",
+          "some Nutaku game",
+          "Just Cause 3 ;)",
+          "with myself ;l",
+          "russian roulette"]
+
+#the message displayed when bother is triggered
+no = "<@YOUR_DISCORD_ID> sum dud wants something from u, idk :/"
+
+#enter the nick names that triggers the no message above
+bother = ["example",
+          "example",
+          "example",
+          "example"]
