183a21857f6ea7456bb71cc1b11780619eaecd36
NOCx420 <56538139+NOCx420@users.noreply.github.com>
2021-09-11 13:52:14 +0200

    Updated config.py
    added variables for mod_channel and invitelnk

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
-status = ["with Anime Boobies",
-          "with your Mom",
+status = ["use -help",
+          "with legos",
           "Deus Ex: Human Revolution",
           "with Roadkill",
           "Cyberbonk2088",
-          "Minecraft on Pornhub",
-          "some Nutaku game",
+          "Minecraft on Roblox",
+          "use -help",
           "Just Cause 3 ;)",
-          "with myself ;l",
+          "use -help",
           "russian roulette"]
 
 #the message displayed when bother is triggered
 no = "<@YOUR_DISCORD_ID> sum dud wants something from u, idk :/"
+
+#The moderation channel to send reports to, its an integer and not a string!
+mod_channel = CHANNEL_ID_HERE
+
+#the link for server invite
+invitelnk = "https://discord.com/invite/YOUR_INVITE"
 
 #enter the nick names that triggers the no message above
 bother = ["example",
