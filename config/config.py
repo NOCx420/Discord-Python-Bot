cb80ec65352d4b366cb1aeb6a88694af96240311
NOCx420 <56538139+NOCx420@users.noreply.github.com>
2022-01-15 14:04:28 +0100

    Before migration backup

--- config/config.py
+++ config/config.py
@@ -1,25 +1,36 @@
-status = ["use -help",
-          "with legos",
+status = ["with Mao ZeDong",
+          "use -help",
+          "with your Mom",
           "Deus Ex: Human Revolution",
+          "use -help",
           "with Roadkill",
           "Cyberbonk2088",
-          "Minecraft on Roblox",
+          "use -help",
+          "Minecraft on Pornhub",
+          "some Nutaku game",
           "use -help",
           "Just Cause 3 ;)",
+          "Battlefield2042",
           "use -help",
           "russian roulette"]
 
 #the message displayed when bother is triggered
-no = "<@YOUR_DISCORD_ID> sum dud wants something from u, idk :/"
+no = "<@605784984409866241> sum dud wants something from u, idk :/"
 
-#The moderation channel to send reports to, its an integer and not a string!
-mod_channel = CHANNEL_ID_HERE
+#The moderation channel to send reports to
+mod_channel = 883023161015279683
 
 #the link for server invite
-invitelnk = "https://discord.com/invite/YOUR_INVITE"
+invitelnk = "https://discord.com/invite/BKwkFdGT6H"
 
 #enter the nick names that triggers the no message above
-bother = ["example",
-          "example",
-          "example",
-          "example"]
+bother = ["patrik",
+          "Patrik",
+          "patrick",
+          "Patrick"]
+
+#makes howgay command always return 0% for this id
+ungayid = 605784984409866241
+
+#link to the image the chonk should display
+chonklnk = "https://cdn.discordapp.com/attachments/885901272937553950/887334771049525359/chonk.png"
